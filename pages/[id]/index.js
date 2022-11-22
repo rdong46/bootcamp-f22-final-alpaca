344b3294ae31add1c2e68ad6fbc634ea35b908bd
rdong46 <rickydongcollege@gmail.com>
2022-11-21 19:50:59 -0500

    update organization

---
+++ pages/[id]/index.js
@@ -0,0 +1,3 @@
+export default function Post() {
+  return <div>Individual Post with Comments Screen</div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
-  return <div>Hello World!</div>;
+  return <div>Display Posts Screen</div>;
 }
